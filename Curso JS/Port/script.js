320a5d2e53263fe05c5e94978b9ad596acaf821c
João Victor <110439244+PK-John@users.noreply.github.com>
2023-04-29 01:25:54 -0300

    Add files via upload

---
+++ Curso JS/Port/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiplo-texto", { 
+    strings : ["Estudante de Programação", "Desenvolvedor Web"] ,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
